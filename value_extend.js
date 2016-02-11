a48cd1211af67e13c3ea8d83cb32a02904e90eee
navyxie <navyxie2010@gmail.com>
2016-02-11 09:47:57 +0800

    添加数值扩展

---
+++ value_extend.js
@@ -0,0 +1,12 @@
+(function (global) {
+  var global_isFinite = global.isFinite;
+
+  Object.defineProperty(Number, 'isFinite', {
+    value: function isFinite(value) {
+      return typeof value === 'number' && global_isFinite(value);
+    },
+    configurable: true,
+    enumerable: false,
+    writable: true
+  });
+})(this);
