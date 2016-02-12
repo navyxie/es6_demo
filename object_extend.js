588b14bf57eb2b8528b906ee5997455127bb0ffb
navyxie <navyxie2010@gmail.com>
2016-02-13 01:39:46 +0800

    add

--- object_extend.js
+++ object_extend.js
@@ -19,3 +19,17 @@ module.exports = {
   setItem: setItem,
   clear: clear
 };
+
+var birth = '2000/01/01';
+
+var Person = {
+
+  name: '张三',
+
+  //等同于birth: birth
+  birth,
+
+  // 等同于hello: function ()...
+  hello() { console.log('我的名字是', this.name); }
+
+};
