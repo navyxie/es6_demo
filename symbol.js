1481915ef27c2c81d33742ab3198c55887fd371f
navyxie <navyxie2010@gmail.com>
2016-02-14 18:31:30 +0800

    add

--- symbol.js
+++ symbol.js
@@ -103,3 +103,13 @@ Object.getOwnPropertyNames(x) // ['0']
 Object.getOwnPropertySymbols(x) // [Symbol(size)]
 //上面代码中，对象x的size属性是一个Symbol值，所以Object.keys(x)、Object.getOwnPropertyNames(x)都无法获取它。这就造成了一种非私有的内部方法的效果。
 
+//需要注意的是，Symbol.for为Symbol值登记的名字，是全局环境的，可以在不同的iframe或service worker中取到同一个值。
+
+iframe = document.createElement('iframe');
+iframe.src = String(window.location);
+document.body.appendChild(iframe);
+
+iframe.contentWindow.Symbol.for('foo') === Symbol.for('foo')
+// true
+//上面代码中，iframe窗口生成的Symbol值，可以在主页面得到。
+
