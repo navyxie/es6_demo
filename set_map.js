f443f56409c86d1facee572cc8cb5e39859c93ac
navyxie <navyxie2010@gmail.com>
2016-02-17 10:35:09 +0800

    set_map

--- set_map.js
+++ set_map.js
@@ -65,3 +65,18 @@ for (let x of set) {
 // red
 // green
 // blue
+
+let a = new Set([1, 2, 3]);
+let b = new Set([4, 3, 2]);
+
+// 并集
+let union = new Set([...a, ...b]);
+// [1, 2, 3, 4]
+
+// 交集
+let intersect = new Set([...a].filter(x => b.has(x)));
+// [2, 3]
+
+// 差集
+let difference = new Set([...a].filter(x => !b.has(x)));
+// [1]
