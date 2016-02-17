76e8b03f014e02cd41986b5f623ef5e62e2debb0
navyxie <navyxie2010@gmail.com>
2016-02-17 10:50:02 +0800

    set_map

--- set_map.js
+++ set_map.js
@@ -103,3 +103,13 @@ ws.has(foo);    // false
 
 ws.delete(window);
 ws.has(window);    // false
+
+var m = new Map();
+var o = {p: "Hello World"};
+
+m.set(o, "content")
+m.get(o) // "content"
+
+m.has(o) // true
+m.delete(o) // true
+m.has(o) // false
