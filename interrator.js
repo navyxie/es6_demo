733aa9d587b2a9f7036217a7fe60a3d439665887
navyxie <navyxie2010@gmail.com>
2016-02-18 12:03:49 +0800

    add

--- interrator.js
+++ interrator.js
@@ -55,3 +55,27 @@ str[Symbol.iterator] = function() {
 
 [...str] // ["bye"]
 str // "hi"
+
+var myIterable = {};
+
+myIterable[Symbol.iterator] = function* () {
+  yield 1;
+  yield 2;
+  yield 3;
+};
+[...myIterable] // [1, 2, 3]
+
+// 或者采用下面的简洁写法
+
+let obj = {
+  * [Symbol.iterator]() {
+    yield 'hello';
+    yield 'world';
+  }
+};
+
+for (let x of obj) {
+  console.log(x);
+}
+// hello
+// world
