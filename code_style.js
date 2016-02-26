4a527a1a6f48526d0a98a4999957435c181c99bd
navyxie <navyxie2010@gmail.com>
2016-02-27 01:18:20 +0800

    add code_style

--- code_style.js
+++ code_style.js
@@ -19,3 +19,14 @@ Object.assign(a, { x: 3 });
 // good
 const a = { x: null };
 a.x = 3;
+
+// bad
+function concatenateAll() {
+  const args = Array.prototype.slice.call(arguments);
+  return args.join('');
+}
+
+// good
+function concatenateAll(...args) {
+  return args.join('');
+}
